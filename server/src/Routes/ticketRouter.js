e0d00e76c6d1f5a7ea1abd70ed3dc85217016903
Debmalya Kuila <debmalyakuila2002@gmail.com>
2024-05-01 22:35:51 +0530

    created add  new ticket endpoint

--- server/src/Routes/ticketRouter.js
+++ server/src/Routes/ticketRouter.js
@@ -1,8 +1,35 @@
 const express=require("express")
+const mongoose=require("mongoose")
+const ticketModel=require("../models/ticketModel")
 const router=express.Router()
 
-router.all("/",(req,res)=>{
-    res.json({message : "hello from ticket router"})
+router.all("/",(req,res,next)=>{
+   // res.json({message : "hello from ticket router"})
+    next()
+})
+
+router.post("/",async(req,res)=>{
+    const {sender,role,subject,description}=req.body
+
+    const ticketObj={
+       clientId: new mongoose.Types.ObjectId('4edd40c86762e0fb12000003'),
+        sender,
+        role,
+        subject,
+        description
+    }
+    
+    try {
+        const newTicket = await new ticketModel(ticketObj)
+        await newTicket.save();
+        if(newTicket._id){
+            return  res.status(201).send({ message:"new ticket created",newTicket})
+        }
+        res.status(400).send({message:"something went wrong.Please try again later..."});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message:"Internal server error"});
+    }
 })
 
 module.exports=router
---
+++ server/src/models/ticketModel.js
@@ -0,0 +1,54 @@
+const {Schema,model}=require("mongoose")
+
+const ticketSchema=new Schema({
+    clientId:{
+        type:Schema.Types.ObjectId
+    },
+    sender:{
+        type:String,
+        maxlength:100,
+        minlength:3,
+        required:true
+    },
+    role:{
+        type:String,
+        maxlength:100,
+        minlength:3,
+        required:true
+    },
+    subject:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String
+    },
+    status:{
+        type:String,
+        maxlength:30,
+        default:"pending"
+    },
+    openAt:{
+        type:Date,
+        default:Date.now(),
+        required:true
+    }
+})
+
+ticketSchema.methods.toJSON = function(){
+    //get the mongoose document using this keyword
+    const ticket = this
+    //convert the mongoose document to an object by using toObject() method of mongoose
+    const ticketObject=ticket.toObject()
+    //delete password , tokens, version from user object to hide private data of user
+    delete ticketObject.openAt
+    delete ticketObject._id
+    delete ticketObject.__v
+    delete ticketObject.clientId
+
+    return ticketObject
+}
+
+const ticketModel=model("ticket",ticketSchema)
+
+module.exports=ticketModel
